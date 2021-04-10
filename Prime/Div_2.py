95315327e034a93ea8122c3cb8f1e6315c8619de
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-04-10 18:37:54 -0400

    Added Subfiles for subdivisions

---
+++ Prime/Div_2.py
@@ -0,0 +1,21 @@
+
+x = int(input("which number?"))
+x = abs(x)
+print(x)
+
+a = x
+b = x
+c = x
+d = x
+
+#divisable by 2?
+while a > 0:
+    a = a - 2
+
+print (a)
+
+if a == -1:
+    print("not 2")
+else:
+    print("div 2")
+
--- Prime/Div_3.py
+++ Prime/Div_3.py
@@ -1,12 +1,3 @@
-
 x = int(input("which number?"))
 x = abs(x)
 print(x)
-
-a = x
-b = x
-c = x
-d = x
-
-
-
