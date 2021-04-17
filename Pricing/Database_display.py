490036aeb970ae503a31d34ac68ca91fb070a0f8
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-04-17 12:36:33 -0700

    added input and row print functions

--- Pricing/Database_display.py
+++ Pricing/Database_display.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv (r"C:\Users\aadit\Documents\Python\.vscode\AP CSP Create Task\AP-CSP-Create-Task\Pricing\Datasets\Public_Garages_or_Parking_Lots - Public_Garages_or_Parking_Lots.csv")
-df = pd.DataFrame(data, columns= ["x","y"])
-print (df) 
+row = int(input("which row?"))
+
+df = pd.read_csv (r"C:\Users\aadit\Documents\Python\.vscode\AP CSP Create Task\AP-CSP-Create-Task\Pricing\Datasets\Public_Garages_or_Parking_Lots - Public_Garages_or_Parking_Lots.csv")
+
+print (df)
+print(df.iloc[row,0:2])
