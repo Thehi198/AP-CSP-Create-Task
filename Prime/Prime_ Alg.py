33661fd5b7cfa2ae914d66e4c07b07c6b436a48b
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-04-19 18:09:15 -0700

    added range function

--- Prime/Prime_ Alg.py
+++ Prime/Prime_ Alg.py
@@ -5,7 +5,13 @@ x = abs(x)
 print(x)
 
 #div value
-val = x/2
-val = int(val)
-print(val)
+n = x/2
+n = int(n)
+print(n)
+
+#find values between 2,n
+num_range = range(2,n)
+print(num_range)
+num_list = list(num_range)
+print(num_list)
 
