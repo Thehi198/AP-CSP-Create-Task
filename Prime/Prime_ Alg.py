d8ff8b61d07b242f77e9465cafb6847364aa10b8
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-05-01 09:25:37 -0700

    leveraging DIv_factors to calcualate prime traits

--- Prime/Prime_ Alg.py
+++ Prime/Prime_ Alg.py
@@ -27,3 +27,20 @@ map_output = map(div_num, num_list)
 output_list = list(map_output)
 print(output_list)
 
+#round up values for output_list
+round_list = [round(num) for num in output_list]
+print(round_list)
+
+#divide rounded by output
+div_output = [i / j for i, j in zip(round_list, output_list)]
+print(div_output)
+
+#count instances of 1 in div_output
+output_occur = div_output.count(1.0)
+print(output_occur)
+
+#if argument detects if list has 1 or not
+if output_occur == 0:
+    print("prime")
+else:
+    print("composite")
