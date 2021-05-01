90596f302e519e66a6b285c20d8aabfbbc2ab8ca
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-04-30 21:00:49 -0700

    Added comments

--- Prime/Prime_ Alg.py
+++ Prime/Prime_ Alg.py
@@ -1,4 +1,4 @@
-#input
+#input and processing
 x = input("which number?")
 x = int(x)
 x = abs(x)
@@ -12,7 +12,11 @@ print(n)
 #find values between 2,n
 num_range = range(2,n)
 num_list = list(num_range)
-print(num_list)
+print("list of numbers between 2 and N:" , num_list)
+
+#count number of elements in list
+num_elem = len(num_list)
+print("Number of elements in num_list:" ,num_elem)
 
 #define Div_Num function
 def div_num(y):
@@ -23,6 +27,3 @@ map_output = map(div_num, num_list)
 output_list = list(map_output)
 print(output_list)
 
-
-
-    
