a7ed7eb96b501e17d63c7a5ee6a37c8c6015a349
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-05-01 11:25:39 -0700

    Removed num element

--- Prime/Prime_ Alg.py
+++ Prime/Prime_ Alg.py
@@ -14,9 +14,6 @@ num_range = range(2,n)
 num_list = list(num_range)
 print("list of numbers between 2 and N:" , num_list)
 
-#count number of elements in list
-num_elem = len(num_list)
-print("Number of elements in num_list:" ,num_elem)
 
 #define Div_Num function
 def div_num(y):
