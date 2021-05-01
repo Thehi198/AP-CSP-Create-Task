b737362c6b5b3b1731e1cd5e3031a9ed177e540f
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-05-01 09:24:30 -0700

    divide by rounded list to leverage division prime

--- Notepad.py
+++
@@ -1,7 +0,0 @@
-#define function for counting number of intigers in list
-type_output = 0
-def checkType(output_list):
-    for element in output_list:
-        if isinstance(element, int):
-            type_output = type_output + 1
-print(type_output)
