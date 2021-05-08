e663405974a77442f85bc7853e59e2fc59534098
Thehi198 <61992425+Thehi198@users.noreply.github.com>
2021-05-08 13:26:02 -0700

    Organized Files

--- Notepad.py
+++ Notepad.py
@@ -1,12 +1,25 @@
-x = input ("array")
-print(x)
-list(x)
+def variance(data):
+    # Number of observations
+    n = len(data)
+    # Mean of the data
+    mean = sum(data) / n
+    # Square deviations
+    deviations = [(x - mean) ** 2 for x in data]
+    # Variance
+    variance = sum(deviations) / n
+    return variance
+...
+print("Enter numbers to calculate. Please seperate values by hiitting enter. Press enter twice or type stop once you are done")
+try:
+    input_list = []
+      
+    while True:
+        input_list.append(int(input()))
+except:
+    print(input_list)
 
-#define Div_Num function
-def div_num(y):
-    return input/y
+list(input_list)
+print(input_list)
 
-#div by values num_list
-map_output = map(div_num, x)
-output_list = list(map_output)
-print(output_list)
+output_array = map(variance, input_list)
+output_list = list(output_array)
